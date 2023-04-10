54fe0eb7aa30da9e1a381989bd0216f563915dfb
CountingMstar <mwh510@naver.com>
2023-04-10 23:22:36 +0900

    text

--- algorithm-interview/2-python/ch06/1-1.py
+++ algorithm-interview/2-python/ch06/1-1.py
@@ -1,3 +1,35 @@
+import time
+
+start = time.time()
+
+text1 = "A man, a plan, a canal: Panama"
+text2 = "race a car"
+
+text = text1
+# text = text2
+strs = []
+for char in text:
+    # print(char)
+    # isalnum() : 영문자, 숫자 여부 판단
+    if char.isalnum():
+        strs.append(char.lower())
+        print(strs)
+
+while len(strs) > 1:
+    # strs.pop(0) : 맨 앞부분 뽑기
+    # strs.pop() : 맨 뒷부분 뽑기
+    if strs.pop(0) != strs.pop():
+        print('False')
+
+    else:
+        print('True')
+    
+end = time.time()
+print(f"{end - start:.5f} sec")
+
+
+
+##############################################
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         strs = []
--- algorithm-interview/2-python/ch06/1-3.py
+++ algorithm-interview/2-python/ch06/1-3.py
@@ -1,6 +1,32 @@
 import re
 
+text1 = "A man, a plan, a canal: Panama"
+text2 = "race a car"
 
+text = text1
+# text = text2
+
+text = text.lower()
+print(text)
+# 불필요한 문제 제거
+# re.sub(불필요한 문자, 대체할 문자, 수정할 문장)
+text = re.sub('[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·]', '', text)
+# 띄어쓰기 공백 지우기
+text = re.sub(r"\s", "", text)
+print('################')
+print(text)
+
+inverse_text = text[::-1]
+print(inverse_text)
+
+if text == inverse_text:
+    print('True')
+else:
+    print('False')
+
+
+    
+##############################################
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
