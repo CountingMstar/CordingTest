e413be18ef94d0c49c2cfa119b933f67901015ce
CountingMstar <mwh510@naver.com>
2023-04-13 23:21:06 +0900

    water

--- algorithm-interview/3-linear-data-structures/ch07/8-1.py
+++ algorithm-interview/3-linear-data-structures/ch07/8-1.py
@@ -1,4 +1,46 @@
 from typing import List
+import time
+
+input = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+input = [2, 0, 1, 0, 3]
+input = [1, 0, 3]
+
+print(input.index(max(input)))
+
+max_index = input.index(max(input))
+window = []
+window.append(input[max_index])
+water = 0 
+#left
+for i in range(max_index):
+    if len(window) == 2:
+        window.remove(window[0])
+    index = max_index - (i + 1)
+    value = input[index]
+    window.append(value)
+    print(window)
+
+    if window[0] < window[1]:
+        wall_index = index + 1
+        print('=============')
+        print(window[1])
+        print(input[wall_index])
+        while True:
+            print('*******')
+            water += (window[1] - input[wall_index])
+            wall_index = index + 1
+            print(window[1])
+            print(input[wall_index])
+            if window[1] > input[wall_index]:
+                break
+
+print('#######')
+
+
+print(water)
+    
+
+
 
 
 class Solution:
