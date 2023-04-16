c9d437369c944bd9084321aa1412e8d3d538deab
CountingMstar <mwh510@naver.com>
2023-04-16 13:32:35 +0900

    list

--- algorithm-interview/3-linear-data-structures/ch08/13-1_linked_list.py
+++ algorithm-interview/3-linear-data-structures/ch08/13-1_linked_list.py
@@ -21,7 +21,7 @@ class LinkedList:
         self.number_of_data += 1
 
 I = LinkedList()
-input = [1, 2]
+input = [10, 20, 30]
 
 for i in input:
     I.append(i)
@@ -29,32 +29,3 @@ for i in input:
 print(I.head.data)
 print(I.head.next.data)
 print(I.head.next.next.data)
-
-
-#########################################
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
-class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
-        q: List = []
-
-        if not head:
-            return True
-
-        node = head
-        # 리스트 변환
-        while node is not None:
-            q.append(node.val)
-            node = node.next
-
-        # 팰린드롬 판별
-        while len(q) > 1:
-            if q.pop(0) != q.pop():
-                return False
-
-        return True
